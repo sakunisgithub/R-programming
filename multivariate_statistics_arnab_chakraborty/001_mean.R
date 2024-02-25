96dd649ebdcf79f3814ba53e77944bdb84285e82
sakunisgithub <ananda2003biswas@gmail.com>
2024-02-26 01:48:17 +0530

    Initial Commit

--- multivariate_statistics_arnab_chakraborty/001_mean.R
+++
@@ -1,3 +0,0 @@
-data <- read.csv("~//Documents//r_programming//multivariate_statistics_arnab_chakraborty//data.csv")
-data
-apply(data, 2, mean)
---
+++ multivariate_statistics_arnab_chakraborty/001_mean/001_mean.R
@@ -0,0 +1,3 @@
+data <- read.csv("~//Documents//r_programming//multivariate_statistics_arnab_chakraborty/001_mean//data.csv")
+data
+apply(data, 2, mean)
