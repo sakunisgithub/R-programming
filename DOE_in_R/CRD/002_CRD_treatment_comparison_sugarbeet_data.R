0c393756b4563ca57c80aadfeaabf78d3e5cd30a
sakunisgithub <ananda2003biswas@gmial.com>
2024-03-10 02:25:46 +0530

    Little Change

--- DOE_in_R/CRD/002_CRD_treatment_comparison_sugarbeet_data.R
+++ DOE_in_R/CRD/002_CRD_treatment_comparison_sugarbeet_data.R
@@ -1,5 +1,5 @@
 library(daewr)
-view(sugarbeet)
+View(sugarbeet)
 mod1_aov <- aov(yield ~ treat, data = sugarbeet)
 summary(mod1_aov)
 con <- matrix(data = c(1, -1/3, -1/3, -1/3, 0, 1, -1, 0, 0, 0, 1, -1), byrow = TRUE, nrow = 3, ncol = 4)
