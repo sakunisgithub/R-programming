6bff542c796ac8151dc4e515e9b65ae30a46f6b6
sakunisgithub <ananda2003biswas@gmial.com>
2024-07-12 02:45:20 +0530

    some additions

--- survival_analysis_lee_wang_exercise_2.1.R
+++ survival_analysis_lee_wang_exercise_2.1.R
@@ -16,11 +16,20 @@ f_t_hat[length(f_t_hat)] = NA
 
 number_of_patients_dying_per_unit_time_in_the_interval <- survival_data$number_dying_in_the_interval / interval_width
 
-h_t_hat <- number_of_patients_dying_per_unit_time_in_the_interval / ( survival_data$number_alive_at_the_beginning_of_the_interval * interval_width)
+h_t_hat_method_1 <- number_of_patients_dying_per_unit_time_in_the_interval / ( survival_data$number_alive_at_the_beginning_of_the_interval * interval_width)
 
-h_t_hat[length(h_t_hat)] = NA
+h_t_hat_method_1[length(h_t_hat_method_1)] = NA
 
-analysis_table <- data.frame(survival_data, t = 0:9, S_t_hat = round(S_t_hat, digits = 3), f_t_hat = round(f_t_hat, digits = 3), h_t_hat = round(h_t_hat, digits = 3))
+h_t_hat_method_2 <- number_of_patients_dying_per_unit_time_in_the_interval / (survival_data$number_alive_at_the_beginning_of_the_interval - survival_data$number_dying_in_the_interval / 2)
+
+h_t_hat_method_2[length(h_t_hat_method_2)] = NA
+
+analysis_table <- data.frame(survival_data, 
+                             t = 0:9,
+                             S_t_hat = round(S_t_hat, digits = 3), 
+                             f_t_hat = round(f_t_hat, digits = 3), 
+                             h_t_hat_method_1 = round(h_t_hat_method_1, digits = 3),
+                             h_t_hat_method_2 = round(h_t_hat_method_2, digits = 3))
 
 View(analysis_table)
 
@@ -41,7 +50,7 @@ analysis_table %>%
   labs(x = "t(years)", y = "Estimated Desnity Function", title = "Plot of Estimated Density Function")
 
 analysis_table %>%
-  ggplot(aes(x = t, y = h_t_hat)) +
+  ggplot(aes(x = t, y = h_t_hat_method_2)) +
   geom_line(linewidth = 1, col = "blue") +
   geom_point(size = 2, col = "red", shape = 4, stroke = 1.5) +
   scale_size_discrete(limits = 0:9) +
