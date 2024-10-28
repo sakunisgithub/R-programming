010763d2d2c61a68b60097dc71af963322b4e4a7
sakunisgithub <ananda2003biswas@gmial.com>
2024-10-28 23:19:40 +0530

    Initial Commit

---
+++ msc_sem_1_practicals/progress_indicator_animation.R
@@ -0,0 +1,64 @@
+# progress indicator by a pie-chart
+for (i in 1:100) {
+  pie(c(i, 100 - i), 
+      c(as.character(i), as.character(100-i)), 
+      col = c("green", "pink"))
+  Sys.sleep(1)
+}
+
+# progress indicator by a box filling up
+plot(NA, NA, 
+     xlim = c(1, 10), 
+     ylim = c(1, 10),
+     xaxt = "n",
+     yaxt = "n",
+     xlab = "",
+     ylab = "")
+axis(side = 1, at = 1:10, labels = 1:10)
+axis(side = 4, at = 1:10, labels = seq(10, 100, 10))
+
+for (j in 1:10) {
+  for (i in seq(1, 10, 1)) {
+    points(x = i, y = j, pch = 19, col = "#23e705", cex = 4)
+    Sys.sleep(1)
+  }
+}
+
+
+# progress indicator by a box filling up in a different way
+plot(NA, NA, 
+     xlim = c(1, 10), 
+     ylim = c(1, 10),
+     xlab = "",
+     ylab = "",
+     xaxt = "n",
+     yaxt = "n")
+axis(side = 1, at = 1:10, labels = 1:10)
+axis(side = 4, at = 1:10, labels = seq(10, 100, 10))
+
+
+for (t in 1:10) {
+  
+  pop <- 1:10
+  
+  for(k in 1:10){
+    
+    if(k < 10){
+      num <- sample(pop, 1)
+    } else{
+      num <- pop[1]
+    }
+    
+    cl <- rep("white", 10-t+1)
+    
+    for (m in 1:(10-t+1)) {
+      cl[m] <- "#059fe7"
+      points(x = rep(num, length(10:t)), y = 10:t, col = cl, pch = 19, cex = 4)
+      Sys.sleep(0.75)
+      cl <- rep("white", 10-t+1)
+    }
+    
+    index <- which(pop == num)
+    pop <- pop[-index]
+  }
+}
