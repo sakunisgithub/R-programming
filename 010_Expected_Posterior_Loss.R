43d3519eb39ad18c90c6399eeb94b785007d248f
sakunisgithub <ananda2003biswas@gmial.com>
2026-03-24 02:19:53 +0530

    Initial Commit

---
+++ 010_Expected_Posterior_Loss.R
@@ -0,0 +1,22 @@
+# a_1
+a <- integrate(f = function(x) (x - 90) * dnorm(x, mean = 110.38, sd = sqrt(69.23)), lower = 90, upper = 110)
+
+b <- integrate(f = function(x) 2 * (x - 90) * dnorm(x, mean = 110.38, sd = sqrt(69.23)), lower = 110, upper = Inf)
+
+a$value + b$value
+
+
+# a_2
+a <- integrate(f = function(x) (90 - x) * dnorm(x, mean = 110.38, sd = sqrt(69.23)), lower = -Inf, upper = 90)
+
+b <- integrate(f = function(x) 2 * (x - 110) * dnorm(x, mean = 110.38, sd = sqrt(69.23)), lower = 110, upper = Inf)
+
+a$value + b$value
+
+
+# a_3
+a <- integrate(f = function(x) 2 * (110 - x) * dnorm(x, mean = 110.38, sd = sqrt(69.23)), lower = -Inf, upper = 90)
+
+b <- integrate(f = function(x) (110 - x) * dnorm(x, mean = 110.38, sd = sqrt(69.23)), lower = 90, upper = 110)
+
+a$value + b$value
